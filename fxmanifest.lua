6ea794443b12eaf8a625a598a70a4731d17eaa7e
cv3tanov <94202270+cv3tanov@users.noreply.github.com>
2026-08-27 00:59:17 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'cvetanov'
+description 'Оптимизиран интерфейс за текуща задача'
+version '2.0.0'
+
+ui_page 'web/ui.html'
+
+files { 'config.lua', 'web/ui.html', 'web/styles.css', 'web/scripts.js' }
+
+client_script 'client.lua'
